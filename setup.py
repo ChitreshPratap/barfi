d13231c8cb5036a99f01b4f3271fe302eff4be02
abhilash <57294516+ChitreshPratap@users.noreply.github.com>
2022-05-02 00:30:37 +0530

    barfi changed to barfy

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='barfy',
     version='1.0.0',
     packages=['test', 'barfy', 'barfy.chitrapat', 'barfy.chitrapat.ext'],
-    url='https://github.com/ChitreshPratap/barfi/blob/main/README.md',
-    license='https://github.com/ChitreshPratap/barfi/blob/main/LICENSE.txt',
+    url='https://github.com/ChitreshPratap/barfy/blob/main/README.md',
+    license='https://github.com/ChitreshPratap/barfy/blob/main/LICENSE.txt',
     author='AbhilashChitresh',
     author_email='',
     description='It is python package which will be used for splash screen in PyQt5 built projects and application.'
