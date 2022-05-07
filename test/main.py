b11c5158e2c4a330225950dbc8005ecbf1f7d03a
abhilash <57294516+ChitreshPratap@users.noreply.github.com>
2022-05-07 17:10:14 +0530

    Load Splash Screen Implemented

--- test/main.py
+++ test/main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from barfy.chitrapat.ext.SplashScreen_DarkEx import SplashScreen_DarkEx
 from barfy.chitrapat.ext.SplashScreen_GrowingEx import SplashScreen_GrowingEx
+from barfy.chitrapat.ext.SplashScreen_LoadEx import SplashScreen_LoadEx
 from barfy.chitrapat.ext.SplashScreen_TransparentEx import SplashScreen_TransparentEx
 from test.TestingWindow import TestingWindow
 
@@ -30,8 +31,17 @@ def splashScreenTransparentEx():
     splash.finish(testWindow)
     app.exec()
 
+def splashScreenLoadEx():
+    app = QApplication(sys.argv)
+    splash = SplashScreen_LoadEx(app,5,appIconPath = r"C:\Users\DELL\Desktop\calendarPng.png",appTitle="Ffilfo",
+                                 loaderGifPath=r"C:\Users\DELL\Downloads\ZZ5H.gif")
+    testWindow=TestingWindow()
+    testWindow.show()
+    splash.finish(testWindow)
+    app.exec()
 
 if __name__ == '__main__':
     # splashScreenDarkDemoEx()
     # splashScreenTransparentEx()
-    splashScreenGrowingEx()
+    # splashScreenGrowingEx()
+    splashScreenLoadEx()
